d1468951d8a732284ba3633c762c218dae574d81
Rocio Dure <rocioevelyndure@gmail.com>
2022-05-20 20:38:00 -0300

    ...

--- components/Nosotros.js
+++ components/Nosotros.js
@@ -2,17 +2,16 @@ import styles from '../styles/nosotros.module.scss';
 export default () => {
 
     return (
-        <div className={styles.container}>
-            <div  id="nosotros" className={styles["container-info"]}>
+        <div id="nosotros" className={styles.container}>
+            <div className={styles.info}>
                 <p>Neuraltech S.A
-                    es una empresa de desarrollo de software de seguridad vial y brinda el equipamiento necesario.
-                    Nuestro objetivo es ampliar nuestra oferta de productos e incorporar nuevas tecnologías para brindar un servicio eficiente con los estándares más altos acordes a la demanda de un sector exigente como el de la seguridad vial.
+                    Es una empresa de desarrollo de software de seguridad vial que  brinda el equipamiento necesario con el objetivo de ampliar nuestra oferta de productos e incorporar nuevas tecnologías para brindar un servicio eficiente con los estándares más altos acordes a la demanda de un sector exigente como el de la seguridad vial.
                 </p>
-            </div>
-
-            <div className={styles["container-img"]}>
-                <img src="images/aboutUs.svg" />
-            </div>
+                </div>
+                <div className={styles["container-img"]}>
+                <img src='images/aboutUs.svg'/>
+                </div>
+          
         </div>
     )
 }
