f0691a46bd09944b91cab466465389ee9290bb70
Rocio Dure <rocioevelyndure@gmail.com>
2022-05-27 10:16:50 -0300

    cambios en nav bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,5 @@
 import styles from '../styles/navbar.module.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
 import {
     faBars
 } from "@fortawesome/free-solid-svg-icons";
@@ -8,26 +7,17 @@ import {
 export default () => {
 
     return (
-        <>
-        <nav className={styles.menu}>
-        <ul>
-                <li><FontAwesomeIcon icon={faBars} /></li>
-                <li><a href='#servicios'>Servicios</a></li>
-                <li><a href="#certificaciones">Certificaciones</a></li>
-                <li><a href='#clientes'>Clientes</a></li>    
-                <li><a href='#contacto'>Contacto</a></li>
-            </ul>
-        </nav>
-        <nav className={styles["nav-bar"]}>
-            <ul>
-                <li><img alt="logo" src='images/logo.jpeg'/></li>
-                <li><a href='#servicios'>Servicios</a></li>
-                <li><a href="#certificaciones">Certificaciones</a></li>
-                <li><a href='#clientes'>Clientes</a></li>    
-                <li><a href='#contacto'>Contacto</a></li>
-            </ul>
+        
+            <nav className={styles["nav-bar"]}>
+                <ul>
+                    <li><img alt="logo" src='images/logo.jpeg' /></li>
+                    <li><a href='#servicios'>Servicios</a></li>
+                    <li><a href="#certificaciones">Certificaciones</a></li>
+                    <li><a href='#clientes'>Clientes</a></li>
+                    <li><a href='#contacto'>Contacto</a></li>
+                </ul>
 
-        </nav >
-        </>
+            </nav >
+        
     )
 }
