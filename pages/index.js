fe9646916aa67af9b3db65d238efb856cf6e119a
Rocio Dure <rocioevelyndure@gmail.com>
2022-06-02 13:25:41 -0300

    cambios en head

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,24 @@ export default function Home() {
     <>
       <Head>
         <title>Neuraltech - Tu aliado en seguridad vial</title>
-        <meta name="description" content="Somos una empresa de desarrollo de software de seguridad vial, brindamos un servicio eficiente con los estándares más altos acordes a la demanda del sector." />
+        <meta name="description" content="Somos una empresa de desarrollo de software de seguridad vial, brindamos un servicio eficiente con los estándares más altos acordes a la demanda del sector" />
         <link rel="icon" href="/images/logo.jpeg" />
+
+<title>Neuraltech - Tu aliado en seguridad vial</title>
+<meta name="title" content="Neuraltech - Tu aliado en seguridad vial"/>
+<meta name="description" content="Somos una empresa de desarrollo de software de seguridad vial, brindamos un servicio eficiente con los estándares más altos acordes a la demanda del sector."/>
+
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://neuraltech.com.ar/"/>
+<meta property="og:title" content="Neuraltech - Tu aliado en seguridad vial"/>
+<meta property="og:description" content="Somos una empresa de desarrollo de software de seguridad vial, brindamos un servicio eficiente con los estándares más altos acordes a la demanda del sector."/>
+<meta property="og:image" content="/images/logo.jpeg"/>
+
+<meta property="twitter:card" content="summary_large_image"/>
+<meta property="twitter:url" content="https://neuraltech.com.ar/"/>
+<meta property="twitter:title" content="Neuraltech - Tu aliado en seguridad vial"/>
+<meta property="twitter:description" content="Somos una empresa de desarrollo de software de seguridad vial, brindamos un servicio eficiente con los estándares más altos acordes a la demanda del sector."/>
+<meta property="twitter:image" content="/images/logo.jpeg" />
       </Head>
 
       <Navbar/>
